e44837da18c16df20febefe2b6716256fad11a63
brigitteorina <brigitteorina@gmail.com>
2023-05-05 10:08:03 +0300

    Getgiphy

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,35 +8,35 @@ function Home() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState({});
 
-  //useEffect(() => {
-   // async function getDetails(url) {
-     // const promise = await fetch(url, {
-      //  method: "GET",
-       // credentials: "same-origin", //include, same-origin
-        //headers: {
-         // Accept: "application/json",
-         // "Content-Type": "application/json",
-        //},
-     // });
-      //const response = await promise.json();
-     // return response;
-    //}
+  useEffect(() => {
+    async function getDetails(url) {
+     const promise = await fetch(url, {
+       method: "GET",
+       credentials: "same-origin", //include, same-origin
+        headers: {
+         Accept: "application/json",
+         "Content-Type": "application/json",
+        },
+      });
+      const response = await promise.json();
+     return response;
+    }
     getDetails("http://localhost:4000/users").then((data) => {
       setUsers(data);
     });
 
-    //const getQuote = async () => {
-      //const promise = await fetch("https://api.quotable.io/random");
+    const getGiphy = async () => {
+      const promise = await fetch("https://api.quotable.io/random");
       
-     // const response = await promise.json();
-      // console.log(response.data);
-     // return response.data;
-    //};
-    //getGiphy().then((res) => {
-     // console.log(res.url);
-      //setData(res);
-    //});
-  //}, []);
+      const response = await promise.json();
+       console.log(response.data);
+      return response.data;
+    };
+    getGiphy().then((res) => {
+      console.log(res.url);
+      setData(res);
+    });
+  }, []);
   function handleSearch(search) {
     setSearch(search);
   }
